31bf52ad350bc884f152d283df40ff2258f13b48
Alexandr <kobelchak@gmail.com>
2025-06-29 13:08:16 +0300

    Add Admin page

---
+++ WebApp/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+[Authorize(Policy = "RequireAdministrator")]
+public class AdminController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
