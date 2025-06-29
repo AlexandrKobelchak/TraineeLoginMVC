efb9b192e45c28c08cff854269db2c2cd2e5cae7
Alexandr <kobelchak@gmail.com>
2025-06-29 10:17:10 +0300

    return DefaultChallengeScheme and addcookie

--- WebApp/Config/ConfigureAuthentication.cs
+++ WebApp/Config/ConfigureAuthentication.cs
@@ -5,11 +5,16 @@ public static partial class Program{
     public static WebApplicationBuilder ConfigureAuthentication(this WebApplicationBuilder builder)
     {
         builder.Services.AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme = "Identity.Application";
-            options.DefaultSignInScheme = "Identity.Application";
-        })
-        .AddIdentityCookies();
+            {
+                options.DefaultAuthenticateScheme = "Identity.Application";
+                options.DefaultSignInScheme = "Identity.Application";
+                options.DefaultChallengeScheme = "Identity.Application";
+            })
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/Account/Login";
+            })
+            .AddIdentityCookies();
          
         return builder;
     }
